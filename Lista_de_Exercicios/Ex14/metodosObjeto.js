ff164d811cda4e5704e1502b1136c73bdebf16b0
rafael Campos <rafael.campos.m@hotmail.com>
2024-03-07 14:56:53 -0300

    Continuação do curso

--- Lista_de_Exercicios/Ex14/metodosObjeto.js
+++ Lista_de_Exercicios/Ex14/metodosObjeto.js
@@ -5,11 +5,37 @@ const carro = {
     modelo: 'Gol',
     ano: 1988,
     cor: 'azul',
-    ligado: false,
-    ligar:function(){
-        if(this.ligado){
-            
+    ligado: true,
+    ligar: function () {
+        if (!this.ligado) {
+            this.ligado = true;
+            return console.log("O carro foi LIGADO");
+        } else if (this.ligado) {
+            return console.log("O carro não pode ser ligado novamente");
+        }
+    },
+    desligar: function () {
+        if (this.ligado) {
+            this.status = true;
+            return console.log("O carro foi desligado");
+        } else if (!this.ligado) {
+            return console.log("Não foi possivel DESLIGAR o carro. Carro já desligado");
         }
     }
 };
 
+function ObterDetalhes() {
+    const status = this.ligado ? 'Ligar' : 'Desligar';
+
+    console.log(`
+    Marca: ${carro.marca},
+    Modelo: ${carro.modelo},
+    Ano: ${carro.ano},
+    Cor: ${carro.cor},
+    Status: ${status}  `);
+};
+ObterDetalhes();
+
+carro.ligar;
+
+ObterDetalhes();
