2439e17e580be542280c9da88bf2d781d9d45f94
rafael Campos <rafael.campos.m@hotmail.com>
2024-03-07 14:10:58 -0300

    Continuação do curso

---
+++ Lista_de_Exercicios/Ex14/enunciado.js
@@ -0,0 +1,8 @@
+/*3 - Crie um arquivo chamado metodosObjeto.js para realizar este exercício.
+
+Utilize o objeto carro do exercício 2 com as propriedades iniciais (marca, modelo, ano, cor) e adicione:
+
+ligado: uma propriedade do tipo booleana que indica se o carro está ligado ou não.
+ligar: um método que imprime no console uma mensagem indicando que o carro está ligado.
+desligar: um método que imprime no console uma mensagem indicando que o carro está desligado. Observação: se o carro já está ligado (ligado = true), não pode ser ligado novamente e vice-versa.
+obterDetalhes: um método que retorna uma string contendo todos os detalhes do carro (marca, modelo, ano, cor, estado de ligado/desligado).*/
---
+++ Lista_de_Exercicios/Ex14/metodosObjeto.js
@@ -0,0 +1,15 @@
+//Ex-3
+
+const carro = {
+    marca: 'Fiat',
+    modelo: 'Gol',
+    ano: 1988,
+    cor: 'azul',
+    ligado: false,
+    ligar:function(){
+        if(this.ligado){
+            
+        }
+    }
+};
+
