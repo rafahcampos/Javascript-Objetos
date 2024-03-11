eeef45a200f67b75b51d3304e28856648a2ca2e7
Rafael Campos <rafael.campos.m@hotmail.com>
2024-03-11 17:42:19 -0300

    Adicionando um exexercicio resolvido

--- Lista_de_Exercicios/Ex14/metodosObjeto.js
+++ Lista_de_Exercicios/Ex14/metodosObjeto.js
@@ -1,32 +1,33 @@
 //Ex-3
 
 const carro = {
-    marca: 'Fiat',
-    modelo: 'Gol',
-    ano: 1988,
-    cor: 'azul',
-    ligado: true,
-    ligar: function () {
-        if (!this.ligado) {
-            this.ligado = true;
-            return console.log("O carro foi LIGADO");
-        } else if (this.ligado) {
-            return console.log("O carro não pode ser ligado novamente");
-        }
-    },
-    desligar: function () {
-        if (this.ligado) {
-            this.status = true;
-            return console.log("O carro foi desligado");
-        } else if (!this.ligado) {
-            return console.log("Não foi possivel DESLIGAR o carro. Carro já desligado");
-        }
+  marca: "Fiat",
+  modelo: "Gol",
+  ano: 1988,
+  cor: "azul",
+  ligado: true,
+  Ligar: function () {
+    if (!this.ligado) {
+      this.ligado = true;
+      return console.log("O carro foi LIGADO");
+    } else if (this.ligado) {
+      return console.log("O carro não pode ser ligado novamente");
     }
+  },
+  Desligar: function () {
+    if (this.ligado) {
+      this.ligado = false;
+      return console.log("O carro foi desligado");
+    } else if (!this.ligado) {
+      return console.log(
+        "Não foi possivel DESLIGAR o carro. Carro já desligado"
+      );
+    }
+  },
 };
 
 function ObterDetalhes() {
-    const status = this.ligado ? 'Ligar' : 'Desligar';
-
+    const status = this.ligado ? 'ligado' : 'desligado';
     console.log(`
     Marca: ${carro.marca},
     Modelo: ${carro.modelo},
@@ -36,6 +37,8 @@ function ObterDetalhes() {
 };
 ObterDetalhes();
 
-carro.ligar;
-
-ObterDetalhes();
+carro.Desligar();
+carro.Ligar();
+carro.Ligar();
+carro.Desligar();
+carro.Desligar();
